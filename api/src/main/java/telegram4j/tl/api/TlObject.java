240740f3007f9b94c1d581e65712c444f49201af
skat <fishmoders@petruchio.org.ru>
2022-02-13 00:53:47 +0300

    Update publish configuration, add missing javadoc, update deps

--- api/src/main/java/telegram4j/tl/api/TlObject.java
+++ api/src/main/java/telegram4j/tl/api/TlObject.java
@@ -3,6 +3,10 @@ package telegram4j.tl.api;
 /** An object of the Telegram API platform. */
 public interface TlObject {
 
-    /** CRC32 hash of the tl object declaration string. */
+    /**
+     * CRC32 hash of the tl object declaration string.
+     *
+     * @return The CRC32 hash of the tl object declaration string.
+     */
     int identifier();
 }
