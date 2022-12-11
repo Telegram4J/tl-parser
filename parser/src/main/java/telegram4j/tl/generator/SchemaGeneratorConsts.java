93573465d8608c4a6e9fd593779a46e6989b43ef
skat <fishmoders@petruchio.org.ru>
2022-12-11 15:03:05 +0300

    Update layer number

--- parser/src/main/java/telegram4j/tl/generator/SchemaGeneratorConsts.java
+++ parser/src/main/java/telegram4j/tl/generator/SchemaGeneratorConsts.java
@@ -19,7 +19,7 @@ final class SchemaGeneratorConsts {
     private SchemaGeneratorConsts() {
     }
 
-    public static final int LAYER = 149;
+    public static final int LAYER = 150;
 
     static final Pattern FLAG_PATTERN = Pattern.compile("^(\\w+)\\.(\\d+)\\?(.+)$");
     static final Pattern VECTOR_PATTERN = Pattern.compile("^[vV]ector<%?([\\w.<>]+)>$");
